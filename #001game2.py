e608064b54204dad511449cb3755e037035de359
CodeAllen <allen5g@qq.com>
2021-03-02 22:27:47 +0800

    add some modify

---
+++ #001game2.py
@@ -0,0 +1,18 @@
+#001game2.py
+
+import random
+secret = random.randint(1,10)
+print('------------------开始------------------')
+temp = input("不妨猜一下我现在心里想的是哪个数字：")
+guess = int(temp)
+while guess != secret:
+    temp = input("哎呀，猜错了，请重新输入吧：")
+    guess = int(temp)
+    if guess == secret:
+        print("哼，猜中了也没有奖励！")
+    else:
+        if guess > secret:
+            print("哥，大了大了~~~")
+        else:
+            print("嘿，小了，小了~~~")
+print("游戏结束，不玩啦^_^")
---
+++ 001game1.py
@@ -0,0 +1,9 @@
+#001game1.py
+temp = input("猜下我心里想的是哪个数字：")
+guess = int(temp)
+if guess == 8:
+    print("恭喜你猜中了！")
+else:
+    print("猜错拉，我心里想的是8！")
+print("游戏结束^_^")
+
